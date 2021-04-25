06045863c9c34e9dd1f8c8f6a1beca18367bcded
Frazix12 <71058095+Frazix12@users.noreply.github.com>
2021-04-25 18:24:30 +0600

    Update playing.js

--- util/playing.js
+++ util/playing.js
@@ -9,7 +9,7 @@ module.exports = {
         const queue = message.client.queue.get(message.guild.id);
         if (!song) {
             sendError(
-                "Leaving the voice channel because I think there are no songs in the queue. If you like the bot stay 24/7 in voice channel go to `util/playing.js` and remove the line number 15\n\nThank you for using my code! [GitHub](https://github.com/SudhanPlayz/Discord-MusicBot)",
+                "Leaving the voice channel because I think there are no songs in the queue. If you like the bot stay 24/7 in voice channel go to `util/playing.js` and remove the line number 15\n\nThank you for using my code! [GitHub](https://github.com/Frazix/Frazix-MusicBot)",
                 message.channel
             );
             message.guild.me.voice.channel.leave(); //If you want your bot stay in vc 24/7 remove this line :D
@@ -66,7 +66,7 @@ module.exports = {
         dispatcher.setVolumeLogarithmic(queue.volume / 100);
 
         let thing = new MessageEmbed()
-            .setAuthor("Started Playing Music!", "https://raw.githubusercontent.com/SudhanPlayz/Discord-MusicBot/master/assets/Music.gif")
+            .setAuthor("Started Playing Music!", "https://raw.githubusercontent.com/Frazix12/Frazix-MusicBot/master/assets/Music.gif")
             .setThumbnail(song.img)
             .setColor("BLUE")
             .addField("Name", song.title, true)
